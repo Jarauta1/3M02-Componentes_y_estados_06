4af4569eef6d51d0cf242a0bc8dcf60d4341c15f
Diego Jarauta <diegojarauta8@gmail.com>
2020-11-26 20:23:03 +0100

    first commit

--- src/footer.js
+++ src/footer.js
@@ -1,8 +1,7 @@
 function Footer(props) {
 
     let indice = parseInt(Math.random()*(3-0))+0
-    console.log(props.aleatorio[indice])
-
+   
 return (<>
 <h1>El libro de oferta:</h1>
 <img src={props.aleatorio[indice].img} width="75"/>
